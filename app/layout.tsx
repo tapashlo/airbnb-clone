744a1843dee14f6c9c821739d8fd24fe464fed4d
Tapashlo <adhikarisandeep321@gmail.com>
2024-04-08 17:45:30 -0500

    already committed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import Modal from "./components/modals/Modal";
 import "./globals.css";
 
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
 
 const font  = Nunito({ subsets: ["latin"] });
 
@@ -19,7 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ClientOnly>
+        <Modal isOpen />
+
         <Navbar />
+
+        </ClientOnly>
         {children}
         </body>
     </html>
